802c533787036f02578ef3552b0d7428c708a22e
Coder-VenkateshS <12venkat21@gmail.com>
2023-06-19 11:21:58 +0000

    constructor and getters setters are added

--- springapp/src/main/java/com/example/springapp/controller/AppController.java
+++ springapp/src/main/java/com/example/springapp/controller/AppController.java
@@ -1,5 +1,28 @@
 package com.example.springapp;
 
 public class AppController {
+    private int a;
+    private int b;
+    
+    
+    public AppController() {
+    }
+    
+    public AppController(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int getA() {
+        return a;
+    }
+    public void setA(int a) {
+        this.a = a;
+    }
+    public int getB() {
+        return b;
+    }
+    public void setB(int b) {
+        this.b = b;
+    }
     
 }
